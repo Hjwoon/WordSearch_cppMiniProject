685d652894baca1e933c9291515061d0f4c639fc
Hjwoon <100463930+Hjwoon@users.noreply.github.com>
2024-01-08 14:36:53 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,14 @@
-#ifndef START_H
-#define START_H
+#include <iostream>
+using namespace std;
 
-class Start {
-public:
-	void removeCursor(); // 커서 숨기기
-	void gotoxy(short x, short y); // 내가 원하는 위치로 커서 옮기기
-	void cls(int bgColor, int txtColor); // 화면 지우고 원하는 배경색으로 설정
-	void init(); // 게임 초기화, 처음 시작과 Restart 때 호출
-	void txtColor(int fgColor, int bgColor); // 텍스트 색상
-	void firstWindows(); // 초기 화면
-	void start(); // 시작 버튼
-	void run();
-};
+#include "Start.h"
+#include "Wordsearch.h"
 
-#endif
+int main() {
+	//Start s;
+	//s.run(); // 초기 화면 시작
+
+	mainGame g;
+	g.run(); // 게임 시작
+
+}
